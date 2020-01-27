646e098c14a0148c0108b670c79d58800d0db07e
Nabeelah <noblenab13@gmail.com>
2020-01-27 15:18:12 +0100

    Create actions for login

---
+++ src/actions/auth.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { apiURL } from '../config';
+
+export const AUTH_LOADING = 'AUTH_LOADING';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const AUTH_FAILURE = 'AUTH_FAILURE';
+
+export const logIn = user => dispatch => {
+  dispatch({ type: AUTH_LOADING });
+  return axios
+    .post(`${apiURL}/api/auth/login`, user)
+    .then(res => {
+      dispatch({ type: LOGIN_SUCCESS, payload: res.data.user });
+    })
+    .catch(err => {
+      dispatch({ type: AUTH_FAILURE, payload: err.response?.data.message });
+    });
+};
