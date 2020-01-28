fe3702aff85e3c748d6c35ef56f53a1f9e35daa3
Nabeelah <noblenab13@gmail.com>
2020-01-28 16:29:07 +0100

    Create global styles and form styles

---
+++ src/styles/Login.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const LoginStyles = styled.div`
+    width: 300px;
+    margin: 0 auto;
+
+    form {
+        button {
+            font-size: 2rem;
+            width: 100%;
+        }
+    }
+`;
