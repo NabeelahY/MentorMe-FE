b417c9f6a8379c926a59433a7a81e98d7679a18a
Nabeelah <noblenab13@gmail.com>
2020-03-10 23:07:43 +0100

    Render chat on screen

--- src/components/Convo/QuestionConvo.js
+++ src/components/Convo/QuestionConvo.js
@@ -4,7 +4,7 @@ import { useParams } from 'react-router-dom';
 import { getQuesConvo, getConvo } from '../../actions/convo';
 import { decodeToken } from '../../utils/checkToken';
 
-const QuestionConvo = ({ getQuesConvo, convos, getConvo }) => {
+const QuestionConvo = ({ getQuesConvo, convos, getConvo, msgs }) => {
   const { id } = useParams();
   useEffect(() => {
     getQuesConvo(id);
@@ -12,17 +12,31 @@ const QuestionConvo = ({ getQuesConvo, convos, getConvo }) => {
 
   let user = decodeToken();
   const { subject } = user;
-  // console.log(subject);
+  console.log(subject);
 
   const quesConvo = convos.find(
     c => c.mentor_id === subject && c.question_id === Number(id)
   );
 
+  console.log(msgs);
+
   useEffect(() => {
     quesConvo && getConvo(quesConvo.id);
   }, [quesConvo, getConvo]);
 
-  return <div>Hello</div>;
+  const { author, messages } = msgs;
+  return (
+    <>
+      {author && (
+        <div>
+          <h1>Chat History with {author.username}</h1>
+          {messages.map(msg => (
+            <p>{msg.text}</p>
+          ))}
+        </div>
+      )}
+    </>
+  );
 };
 
 const mapStateToProps = ({ convoReducer }) => ({
