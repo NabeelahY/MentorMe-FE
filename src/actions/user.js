a6912ac4ad03469007259d3f1b4759a2f806cfa8
Nabeelah <noblenab13@gmail.com>
2020-03-17 11:01:41 +0100

    Create action creators and reducers for getting user chats

--- src/actions/user.js
+++ src/actions/user.js
@@ -6,8 +6,7 @@ export const FETCH_USER_LOADING = 'FETCH_USER_LOADING';
 export const FETCH_USER_SUCCESS = 'FETCH_USER_SUCCESS';
 export const EDIT_USER_SUCCESS = 'EDIT_USER_SUCCESS';
 export const FETCH_USER_FAILURE = 'FETCH_USER_FAILURE';
-
-
+export const FETCH_USER_CONVO_SUCCESS = 'FETCH_USER_CONVO_SUCCESS';
 
 export const getUser = () => async dispatch => {
   let { subject } = decodeToken();
@@ -37,4 +36,18 @@ export const editUser = user => dispatch => {
         payload: err.response?.data.message
       });
     });
+};
+
+export const getUserConvo = () => async dispatch => {
+  let { subject } = decodeToken();
+  dispatch({ type: FETCH_USER_LOADING });
+  try {
+    let convos = await axios.get(`${apiURL}/api/conversations/user/${subject}`);
+    dispatch({ type: FETCH_USER_CONVO_SUCCESS, payload: convos.data });
+  } catch (error) {
+    dispatch({
+      type: FETCH_USER_FAILURE,
+      payload: error.response?.data.message
+    });
+  }
 };
